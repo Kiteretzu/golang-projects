d02f769dfafec641d1fc36706ffa5a652f0aeea4
smarth <smarthverma2003@gmail.com>
2026-03-07 23:35:33 +0530

    go dockerization

---
+++ 06_go-docker/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	println("Server listening on port 8081")
+	http.ListenAndServe(":8081", nil)
+}
