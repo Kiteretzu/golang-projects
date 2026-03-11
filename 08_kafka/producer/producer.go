9f7ba08d66893e45afe09c5867c774f65a42e320
smarth <smarthverma2003@gmail.com>
2026-03-11 23:33:35 +0530

    kafka folder init

---
+++ 08_kafka/producer/producer.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/gofiber/fiber"
+)
+
+type Comment struct {
+	Text string `form: "text" json: "text"`
+}
+
+func main() {
+	app := fiber.New()
+
+	api := app.Group("/api/v1")
+	api.Post("/comments", createComment)
+	app.Listen(":3000")
+
+}
+
+func createComment(c *fiber.Ctx) error {
+	cmt := new(Comment)
+
+	if err := c.BodyParser(cmt); err != nil {
+		c.Status(400).JSON(fiber.Map{
+			"success": false,
+			"message": err,
+		})
+		return err
+	}
+
+	cmtInBytes, err := json.Marshal(cmt)
+
+	PushCommentToQueue("comments", cmtInBytes)
+
+	if err := c.JSON(fiber.Map{
+		"success": true,
+		"message": "Comment pushed successfully",
+		"comment": cmt,
+	}); err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"success": false,
+			"message": "Error creating comment",
+		})
+
+	}
+
+	return nil
+}
